ebf639d455931f1b4bd510538276dce456462b9c
IvanRigs <ivanrios0810@gmail.com>
2024-09-30 13:57:47 -0700

    home posters

--- riskleis/home/home.js
+++ riskleis/home/home.js
@@ -3,8 +3,28 @@ const { createApp, ref } = Vue;
 createApp({
     setup() {
         const apiKey = 'f8c72830b3e50f4fdd3857dea8cb5d97';
+        const sessionId = localStorage.getItem('sessionId');
         const topPeliculas = ref([]);
         const backdrops = ref([]);
+        const imgPATH = ref('');
+        const tieneImagen = ref(false);
+
+        const tendencias = ref([]);
+        const populares = ref([]);
+        const peliculasGratis = ref([]);
+        const series = ref([]);
+
+        const obtenerTendencias = () => {
+            const url = `https://api.themoviedb.org/3/trending/all/week?api_key=${apiKey}`;
+
+            fetch(url)
+                .then(response => response.json())
+                .then(data => {
+                    tendencias.value = data.results || [];
+                })
+                .catch(error => console.error('Error al obtener tendencias:', error));
+        };
+
 
         const obtenerTopPeliculas = () => {
             const url = `https://api.themoviedb.org/3/movie/top_rated?api_key=${apiKey}&language=es-ES&page=1`;
@@ -27,11 +47,39 @@ createApp({
                 .catch(error => console.error('Error:', error));
         };
 
+        const obtenerPerfilUsuario = () => {
+            if (!sessionId) {
+                console.error('No se encontró la session_id.');
+                return;
+            }
+
+            const url = `https://api.themoviedb.org/3/account?session_id=${sessionId}&api_key=${apiKey}`;
+
+            fetch(url)
+                .then(response => response.json())
+                .then(data => {
+                    if (data && data.avatar && data.avatar.gravatar && data.avatar.gravatar.hash) {
+                        imgPATH.value = `https://secure.gravatar.com/avatar/${data.avatar.gravatar.hash}.jpg?s=64`;
+                        tieneImagen.value = true;
+                    } else {
+                        tieneImagen.value = false;
+                    }
+                })
+                .catch(error => console.error('Error al obtener el perfil del usuario:', error));
+        };
+
         obtenerTopPeliculas();
+        obtenerTendencias();
+
 
         return {
             topPeliculas,
             backdrops,
+            imgPATH,
+            tieneImagen,
+            tendencias,
+            populares,
+            series
         }
     }
 }).mount('#app');
